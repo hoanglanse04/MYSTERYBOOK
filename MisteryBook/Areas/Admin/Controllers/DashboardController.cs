b83359066c861c3c1c2afb308081046f0f83b9ac
hoanglanse04 <hoanglando15@gmail.com>
2024-12-19 20:49:06 +0700

    init prj

---
+++ MisteryBook/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MisteryBook.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("Admin")]
+        [Route("admin/dashboard")]
+        public IActionResult Index()
+        {
+            
+            return View("~/Areas/Admin/Views/DashBoard/Index.cshtml");
+        }
+    }
+}
---
+++ MisteryBook/Program.cs
@@ -0,0 +1,36 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Thêm dịch vụ MVC vào dự án
+builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+// Cấu hình các middleware cho ứng dụng
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Cấu hình Routing cho các Area
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}" // Cấu hình Route cho Area
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}" // Route mặc định cho controller và action
+);
+
+app.Run();
